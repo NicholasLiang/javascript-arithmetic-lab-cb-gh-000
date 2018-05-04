0b041780e074ceddf6026747ac4b29ec751f9121
Nicholas Liang <ssss021418@gmail.com>
2018-05-04 16:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ var dec = (n) => n - 1
 var makeInt = (str) => parseInt(str, 10)
 var preserveDecimal = (str) => parseFloat(str)
 
-
-// describe('preserveDecimal(n)', function() {
-//   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-//     expect(preserveDecimal('2.222')).toBe(2.222)
-//   })
-
-//   it('returns NaN as appropriate', function() {
-//     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-//   })
-// })
