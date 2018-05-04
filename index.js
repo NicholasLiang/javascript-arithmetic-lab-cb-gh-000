e8c7f6a57968966d5fd49b8e14ac0e6360b65182
Nicholas Liang <ssss021418@gmail.com>
2018-05-04 16:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var subtract = (a,b) => a - b
 var multiply = (a,b) => a * b
 var divide = (a,b) => a / b
 var inc = (a,b) => a + 1
-var 
+var dec = 
 
 
 // it('inc(n) increments n and returns the result', function() {
